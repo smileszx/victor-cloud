5a1576a30927cc75ff094073083e45c739897593
smile <smile.szx@outlook.com>
2019-09-15 22:30:00 +0800

    add jedis

--- victor-redis/src/main/java/com/micro/redis/JedisTest.java
+++ victor-redis/src/main/java/com/micro/redis/JedisTest.java
@@ -21,6 +21,7 @@ public class JedisTest {
         jedis.set("zhejiangweishi", "中国好声音");
         jedis.hset("lironghao","李凡一","你的酒馆对我打了烊");
         jedis.set("28", "二八定律");
+        jedis.sadd("DevOps", "开发", "运维");
 
         LOGGER.error(String.valueOf(jedis.keys("*")));
 
