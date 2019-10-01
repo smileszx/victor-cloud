b286f215b91829434ed59843ee8b27dcd875b1de
smile <smile.szx@outlook.com>
2019-10-01 22:15:15 +0800

    common msg

--- victor-redis/src/main/java/com/micro/redis/demo/ResultMsg.java
+++ victor-redis/src/main/java/com/micro/redis/demo/ResultMsg.java
@@ -1,10 +1,14 @@
 package com.micro.redis.demo;
 
+import lombok.Data;
+
 /**
- * @Description TODO
+ * @Description
+ * 返回信息统一格式
  * @Author victor su
  * @Date 2019/9/19 22:10
  **/
+@Data
 public class ResultMsg {
 
     private Integer code;
@@ -12,37 +16,4 @@ public class ResultMsg {
     private String msg;
 
     private Object data;
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "ResultMsg{" +
-                "code=" + code +
-                ", msg='" + msg + '\'' +
-                ", data=" + data +
-                '}';
-    }
 }
